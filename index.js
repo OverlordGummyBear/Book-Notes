5ba965bba872e307ead582d107c0702b7e5dc1e2
DANIELSTATIONÆR\danie <danielwlaursen@gmail.com>
2026-07-02 09:06:46 +0200

    Setting up project

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import express from "express" 
+import express from "express"; 
+import axios from "axios";
+import pg from "pg";
+import bodyParser from "body-parser";
+
 const app = express(); //create an app using the express object
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, () => { 
 console.log(`Server running on port ${port}`);
